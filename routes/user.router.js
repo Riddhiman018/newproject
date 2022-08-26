44e382c3e2a8e56b302ffcd4e7545875deb5dbf1
Riddhiman <shubro17@outlook.com>
2022-08-26 23:08:10 +0530

    Modification made to Vehicle Type Condition

--- routes/user.router.js
+++ routes/user.router.js
@@ -166,7 +166,7 @@ router.post('/sendData',async (req,res)=>{
                     } catch (error) {
                         console.log(error)
                         res.status(404).send({
-                            Message:'Unknown Error has occurred'
+                            Message:'Incorrect Data Entered'
                         })
                     }
                 }
